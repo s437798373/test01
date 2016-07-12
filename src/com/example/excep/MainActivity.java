ef603db6122e06acb4f7113d2245f16c1ae9a448
shenyang <437798373@qq.com>
2016-07-12 10:28:34 +0800

    邓子桥是傻逼

--- src/com/example/excep/MainActivity.java
+++ src/com/example/excep/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				Log.e("tag", "----hehehe-");
+				Log.e("tag", "----µË×ÓÇÅÊÇÉµ±Æ-");
 				tv.setText(1);
 			}
 		});
