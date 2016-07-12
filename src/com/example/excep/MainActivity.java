329de2dbd83f75deb2362eb3b0c12a03a1af5227
shenyang <437798373@qq.com>
2016-07-12 14:44:31 +0800

    绑定ID

--- src/com/example/excep/MainActivity.java
+++ src/com/example/excep/MainActivity.java
@@ -2,20 +2,19 @@ package com.example.excep;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	TextView tv;
-	Button bt2;
+	Button bt,bt2;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		MyApplication.getInstance().addActivity(this);
+		bt = (Button) findViewById(R.id.bt);
+		bt2 = (Button) findViewById(R.id.bt2);
 		System.out.println("fdsafd");
 	}
 }
